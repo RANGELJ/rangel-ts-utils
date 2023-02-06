fca160a56e1115efabc4a414fc4e1f85a7ea590d
Jorge Rangel <jorgerangel@192.168.1.3>
2023-02-06 09:39:59 -0600

    Add lib to create deploy scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,41 +1,11 @@
 // @ts-check
-import { execa } from 'execa'
-import fs from 'fs-extra'
-import path from 'path'
+import tsBuildAndCopyPackageFiles  from 'rangel-base-deploy-scripts/es6/tsBuildAndCopyPackageFiles.js'
 import { rootDir, distDir } from './relevantPaths.js'
 
 const main = async () => {
-    console.log('Compiling typescript files...', rootDir)
-
-    await fs.remove(distDir)
-
-    await fs.copy(path.resolve(rootDir, 'package.json'), path.resolve(distDir, 'package.json'))
-    await fs.copy(path.resolve(rootDir, 'package-lock.json'), path.resolve(distDir, 'package-lock.json'))
-
-    const commonjsDir = path.resolve(distDir, 'commonjs')
-
-    await execa('npx', [
-        'tsc',
-        '--outDir',
-        commonjsDir,
-        '--module',
-        'commonjs',
-    ], {
-        cwd: rootDir,
-        stdio: 'inherit',
-    })
-
-    const es6Dir = path.resolve(distDir, 'es6')
-
-    await execa('npx', [
-        'tsc',
-        '--outDir',
-        es6Dir,
-        '--module',
-        'ES6',
-    ], {
-        cwd: rootDir,
-        stdio: 'inherit',
+    await tsBuildAndCopyPackageFiles({
+        targetDir: distDir,
+        sourceDir: rootDir,
     })
 }
 
