6ff73261d749f448dd32c56e76330ef549728d4d
Jorge Rangel <jorgerangel@192.168.1.3>
2023-02-19 20:00:36 -0600

    Update version number

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -14,16 +14,26 @@ const main = async () => {
     const packageDir = path.resolve(sourceDir, 'package.json')
     await fs.copy(packageDir, path.resolve(targetDir, 'package.json'))
 
+    const declarationDir = path.resolve(targetDir, 'types')
+
     await execa('npx', [
         'tsc',
         '--declaration',
         '--emitDeclarationOnly',
         '--outDir',
-        path.resolve(targetDir, 'types'),
+        declarationDir,
     ], {
         cwd: sourceDir,
         stdio: 'inherit',
     })
+
+    const declarationFiles = await fs.readdir(declarationDir)
+
+    await Promise.all(declarationFiles.map(async (declarationFileName) => {
+        const declarationFilePath = path.resolve(declarationDir, declarationFileName)
+        const declarationFileSource = (await fs.readFile(declarationFilePath)).toString()
+        await fs.writeFile(declarationFilePath, declarationFileSource.replace('export default', 'export ='))
+    }))
 
     await execa('npx', [
         'tsc',
